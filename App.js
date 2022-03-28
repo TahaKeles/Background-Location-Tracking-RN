32401fc8a774828142cb1829f1acf06cca2aec3c
taha_keles <kelesstaha@gmail.com>
2022-03-28 19:11:20 +0300

    geolocation integrated

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import MapView, {PROVIDER_GOOGLE, Marker, LatLng} from 'react-native-maps';
 const App = () => {
+  const [coord, setCoord] = useState(LatLng);
+  let initialRegion = {
+    latitude: 39.890622,
+    longitude: 32.793109,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02,
+  };
+  Geolocation.getCurrentPosition(
+    c => {
+      setCoord({
+        latitude: c.coords.latitude,
+        longitude: c.coords.longitude,
+      });
+    },
+    error => console.log(error),
+    {
+      enableHighAccuracy: true,
+    },
+  );
+
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
       style={{flex: 1}}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+      initialRegion={initialRegion}>
+      {coord !== undefined && <Marker coordinate={coord} />}
+    </MapView>
   );
 };
 export default App;
