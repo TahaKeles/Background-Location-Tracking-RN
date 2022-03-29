a48943eacf5aba361ea66f1617eb71425a1f84bb
taha_keles <kelesstaha@gmail.com>
2022-03-29 15:02:28 +0300

    stack navigator added

---
+++ pages/Trippage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Text, SafeAreaView} from 'react-native';
+
+const Trippage = () => {
+  return (
+    <View>
+      <Text>KAKAKAKAKAKAKAKA</Text>
+    </View>
+  );
+};
+
+export {Trippage};
